68f6ebd34a584e8dbd4cccdfd4fb4711d447110b
Reema Asker <reema1822000@gmail.com>
2023-06-25 03:10:12 +0300

    put common attributes and behaviours in QuizBrain class

--- lib/modules/quiz_brain.dart
+++ lib/modules/quiz_brain.dart
@@ -1,1 +1,50 @@
-abstract class Quiz_Brain {}
+import 'package:multi_quiz_s_t_tt9/modules/question.dart';
+
+abstract class QuizBrain {
+  int _question_number = 0;
+  List<Question> questionBank = [];
+
+  String getQuestionText() {
+    return questionBank[_question_number].question_Text;
+  }
+
+  @override
+  setQuestionList();
+  bool getQuestionAnswer_true_or_false();
+
+  int getQuestionAnswer_multiple();
+
+  void nextQuestion() {
+    if (_question_number < questionBank.length - 1) {
+      _question_number++;
+    }
+  }
+
+  bool isFinished() {
+    if (_question_number >= questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  int getQuestionNum() {
+    return _question_number;
+  }
+
+  int getquestionsLength() {
+    return questionBank.length;
+  }
+
+  bool isLastQuestion() {
+    if (_question_number >= questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _question_number = 0;
+  }
+}
